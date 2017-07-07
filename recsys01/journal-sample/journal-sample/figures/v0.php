ae19f1eab14b53b7b98910ed875d1014a5058cc2
Maral A <maral_hd@yahoo.com>
2017-07-07 14:36:17 -0500

    ase

---
+++ recsys01/journal-sample/journal-sample/figures/v0.php
@@ -0,0 +1,20 @@
+
+1. $a = $_POST['inputA'];
+2. $b = $_POST['inputB'];
+3. if ($a < 12)
+   {
+4. 	$b = 5;
+5. 	$a = a-1; 
+   }	
+   else
+6. 	$b = b+3;
+7. if ($a > 7)
+   {
+8. 	if ($b == 5)
+9. 		echo "a\n";
+   	else
+10.		$b= 7;
+   }
+11. echo "$b\n";
+12. echo "done processing\n";
+
